a217bc969dd7ff408a460160e0872a2b2cdfd852
lupo tobi <daniellupo30@gmail.com>
2025-04-06 08:28:16 +0100

    update favicon paths in index.html and configure build options in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,14 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/portfolio/', // Ensure this matches your GitHub repository name
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    rollupOptions: {
+      input: {
+        main: './index.html',
+      },
+    }
+  },
 })
